1c4ed4b28675f95349383ea4e65f22a1dc3a42d7
Elias Ares <eliasares13@gmail.com>
2021-11-24 17:24:57 -0300

    archivo de credenciales añadido

---
+++ setup.bat
@@ -0,0 +1,9 @@
+git config --global user.email "eliasares13@gmail.com"
+
+git config --global user.name "Elías Ares"
+
+git config --global credential.helper store
+
+git config --global push.default simple
+
+git config --global http.postBuffer 524288000
